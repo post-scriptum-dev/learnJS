c491b6d13fe11281a61c209a55ce4f4494112c30
post-scriptum-dev <68875119+post-scriptum-dev@users.noreply.github.com>
2020-11-21 21:14:50 +0300

    Update forecast.js

--- weather-app/js/forecast.js
+++ weather-app/js/forecast.js
@@ -3,7 +3,7 @@ const key = 'AsDpS7mfLetcAgD2rQn8sWdjesm23yep';
 // get city info
 const getCity = async (city) => {
 
-  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
@@ -15,7 +15,7 @@ const getCity = async (city) => {
 // get weather info
 const getWeather = async (id) => {
 
-  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query);
