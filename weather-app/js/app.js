5f42b5e161ee9a67120579aae68eeb027eb38d6b
post-scriptum-dev <68875119+post-scriptum-dev@users.noreply.github.com>
2020-11-21 20:40:39 +0300

    Updating app.js and img
    Add UI funcctions

--- weather-app/js/app.js
+++ weather-app/js/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
   // destructure properties
@@ -16,7 +18,14 @@ const updateUI = (data) => {
     </div>
   `;
 
-  // remove d-none class if present
+  // update the night/day & icon images
+  const iconSrc = `icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+
+  let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+  time.setAttribute('src', timeSrc);
+
+  // remove the d-none class if present
   if(card.classList.contains('d-none')){
     card.classList.remove('d-none');
   };
@@ -44,4 +53,4 @@ cityForm.addEventListener('submit', e => {
   updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err));
-})
+});
